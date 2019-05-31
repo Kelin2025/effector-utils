ccfbe09742699afdf9f684f83a0def293b968f9e
Anton Kosykh <kelin2025@yandex.ru>
2019-05-31 11:31:23 +0300

    fix: Fixed return undefined in `find` method

--- src/find/index.js
+++ src/find/index.js
@@ -2,5 +2,11 @@ import { combine } from "effector"
 import { curryN } from "../_utils/curry"
 
 export const find = curryN(3, (cb, data, list) =>
-  combine(data, list, (data, list) => list.find((...args) => cb(data, ...args)))
+  combine(data, list, (data, list) => {
+    const res = list.find((...args) => cb(data, ...args))
+    if (res === undefined) {
+      return null
+    }
+    return res
+  })
 )
