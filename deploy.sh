c5ae1b32af788e7b135bde2b558ed8346997cf85
Anton Kosykh <kelin2025@yandex.ru>
2019-05-31 12:18:17 +0300

    Deploy config

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:kelin2025/frameninja-utils.git master:gh-pages
+git push -f git@github.com:kelin2025/effector-utils.git master:gh-pages
 
 cd -
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: "Effector Utils",
+  description: "Utilities library on top of Effector",
+  themeConfig: {
+    sidebar: [["/", "Homepage"], ["/methods", "Methods"]],
+    sidebarDepth: 2
+  },
+  base: "/effector-utils"
+}
